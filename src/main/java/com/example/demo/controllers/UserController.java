74a0b1d84a893c174739964dbf3f6a3aa55440f5
Klaudia <klaudia.goralska95@gmail.com>
2019-11-30 14:43:02 +0100

    ADD spring-web dependency

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bookStore/user")
+@CrossOrigin
+
+@AllArgsConstructor
+public class UserController {
+}
