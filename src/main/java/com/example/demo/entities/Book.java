e93842dc0b113ca73a8403bec72c8ff44e1b95c2
Klaudia <klaudia.goralska95@gmail.com>
2019-11-30 13:37:51 +0100

    ADD book entity

---
+++ src/main/java/com/example/demo/entities/Book.java
@@ -0,0 +1,30 @@
+package com.example.demo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="book")
+
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id")
+    private List<User> users = new ArrayList<>();
+
+    private String title;
+    private String author;
+    private String ISBN;
+}
