da1214279b1c982d4743205fb249f2f4a3986850
Klaudia <klaudia.goralska95@gmail.com>
2019-12-03 17:08:04 +0100

    DELETE unnecessary entities

--- src/main/java/com/example/demo/messages/UserMessages.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.messages;
-
-public class UserMessages {
-
-    public static String SAVED_USER = "Added new user: \n";
-
-    public static String GET_USER_BY_ID = "Getting user with id: ";
-}
